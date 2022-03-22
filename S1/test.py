830fe2cddff1f2c28942279cd0de1f9e7552e1a0
Samfisheryu <154616319@qq.com>
2022-03-22 13:36:05 -0400

    uploading S3

--- S1/test.py
+++ S1/test.py
@@ -1,14 +1,24 @@
 import numpy as np
 aa=[1,2,3,4,5]
 a = [0.64,0,0,0.32,0]
+c = np.asarray(a)
+c = np.asarray(c)
 b= np.zeros(6)
+for i in a:
+    if i == 0:
+        del i
+del a[2]
+print(c)
+print(a)
+c=c.tolist()
+print(c)
 res = [idx for idx, val in enumerate(a) if val == 0]
 b[res]=1
 aa=np.array(aa)
 
-print(res)
-print(aa[res[0:int(len(res)*0.8)]])
+#print(res)
+#(aa[res[0:int(len(res)*0.8)]])
 #aa=list(aa[res[0:int(len(res)*0.8)]])
-print(aa)
+#print(aa)
 a.extend(list(aa[res[0:int(len(res)*0.8)]]))
-print(a)
+#print(a)
